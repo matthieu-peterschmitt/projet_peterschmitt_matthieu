6a863088015e3c501ed8fa67d880a65d704d44a6
Matthieu Peterschmitt <matthieu@peterschmitt.eu>
2026-01-15 16:21:42 +0100

    Update API URL in production environment

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projet-peterschmitt-matthieu-api-latest.onrender.com/api',
+  apiUrl: 'https://projet-peterschmitt-matthieu-api.onrender.com/api',
 };
